3da5a76d2a2a8f55e5e1b12e028be2421ceacbf6
Lucas-Jimenez-Uribe <uribelucas06@gmail.com>
2025-02-04 17:46:06 -0500

    primera version

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,2 +1,28 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-alert('el codigo funciona');
+let amigos = []; //lista de amigos
+console.log(amigos)
+
+
+function asignarTextoElemento (elemento, texto){
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML = texto;
+    return
+}
+
+
+function agregarAmigo(){
+    let amigo= document.getElementById('amigo').value;
+
+    if (amigo.trim() !== "") {
+      // Verificar que no esté vacío
+      if (amigos.includes(amigo)) {
+        alert("Este nombre ya está en la lista de amigos.");
+      } else {
+        amigos.push(amigo); // Agregar el nombre al array
+        console.log("Lista de amigos:", amigos); // Mostrar la lista en consola
+      }
+    } else {
+      alert("Por favor, inserte un nombre.");
+    }
+
+    document.getElementById('amigo').value = ""; // Limpiar el input
+}
