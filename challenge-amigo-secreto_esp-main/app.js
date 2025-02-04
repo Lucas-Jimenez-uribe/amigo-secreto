04a37b76688c780dcc106780963dd62bccb3ab62
Lucas-Jimenez-Uribe <uribelucas06@gmail.com>
2025-02-04 17:57:56 -0500

    version dos, comparando todo en minusculas

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -13,13 +13,14 @@ function agregarAmigo(){
     let amigo= document.getElementById('amigo').value;
 
     if (amigo.trim() !== "") {
+        let amigoLowerCase = amigo.toLowerCase();
       // Verificar que no esté vacío
-      if (amigos.includes(amigo)) {
+      if (amigos.some(existingAmigo => existingAmigo.toLowerCase() === amigoLowerCase)) {
         alert("Este nombre ya está en la lista de amigos.");
-      } else {
+    } else {
         amigos.push(amigo); // Agregar el nombre al array
         console.log("Lista de amigos:", amigos); // Mostrar la lista en consola
-      }
+    }
     } else {
       alert("Por favor, inserte un nombre.");
     }
